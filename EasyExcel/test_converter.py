8bcdd7c7b238b646f5dcb19de09a598fc6bf5a9c
Joshua Humphrey <JCHumphrey@drbsystems.com>
2018-10-05 09:42:54 -0400

    Adding spreadsheet creation into tests

--- EasyExcel/test_converter.py
+++ EasyExcel/test_converter.py
@@ -1,19 +1,51 @@
 import unittest
 import converter
 import datetime
+import xlsxwriter
 
 class Test_Converter(unittest.TestCase):
 
+    def CreateSpreasheet(self, filename):
+        # Create a workbook and add a worksheet.
+        workbook = xlsxwriter.Workbook(filename)
+        worksheet = workbook.add_worksheet()
+
+        # Some data we want to write to the worksheet.
+        expenses = (
+            ['Rent', 1000],
+            ['Gas',   100],
+            ['Food',  300],
+            ['Gym',    50],
+        )
+
+        # Start from the first cell. Rows and columns are zero indexed.
+        row = 0
+        col = 0
+
+        # Iterate over the data and write it out row by row.
+        for item, cost in (expenses):
+            worksheet.write(row, col,     item)
+            worksheet.write(row, col + 1, cost)
+            row += 1
+
+        # Write a total using a formula.
+        worksheet.write(row, 0, 'Total')
+        worksheet.write(row, 1, '=SUM(B1:B4)')
+
+        workbook.close()
+
     def test_query_column(self):
+        self.CreateSpreasheet('test_sheet.xlsx')
         test_converter = converter.PythonExcelConverter('test_sheet', '.xlsx', 'Sheet1','test_sheet')
-        expected = ['John Smith', 'Sarah Kay', 'Mr. Developer', 'Patrick Stewart']
+        expected = ['Gas', 'Food', 'Gym', 'Total']
         actual = list(test_converter.query_column(0))
         self.assertEqual(expected, actual)
 
     def test_query_row(self):
+        self.CreateSpreasheet('test_sheet.xlsx')
         test_converter = converter.PythonExcelConverter('test_sheet', '.xlsx', 'Sheet1','test_sheet')
-        expected = ['John Smith', datetime.datetime(1994, 9, 1, 0, 0), 3, 7.4]
-        actual = list(test_converter.query_row(0))
+        expected = ['Food', 300]
+        actual = list(test_converter.query_row(1))
         self.assertEqual(expected, actual)
 
 
